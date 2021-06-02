b91346e0a66b48b6cf57b0e7dcbea49681948b0f
Paulo Vaz <psgvaz@gmail.com>
2021-06-02 17:13:42 -0300

    First Commit

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,20 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    console.log("wwwwwwwww");
+    try {
+      const user = this.turnUserAdminUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      console.log(user);
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    console.log("222222222", user, user_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const admin = this.usersRepository.turnAdmin(user);
+
+    return admin;
   }
 }
 
